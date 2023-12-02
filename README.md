8e0787a847cf4bba0c61392b70c0bbeac1c1d72a
Christopher Milian <christophermilian16@gmail.com>
2023-12-01 23:24:18 -0600

    Feat: request headers are parsed

---
+++ README.md
@@ -0,0 +1,28 @@
+## Description
+
+A Node.js header parser
+
+## Available Endpoints
+
+1. **GET**: `http://localhost:3000/`
+  - App example page. 
+2. **GET**: `http://localhost:3000/api/whoami`
+
+## Examples
+TBD
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+```
+
+## License
+[MIT licensed](LICENSE)
---
+++ src/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+/**
+ * Middleware function to log request information
+ */
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} -${req.ip}`);
+  next();
+});
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', function (req, res) {
+  const responseObj = { 
+    ipaddress: req.ip, 
+    language: req.headers["accept-language"], 
+    software: req.headers["user-agent"] 
+  };
+  return res.json(responseObj);
+});
+
+// listen for requests
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
